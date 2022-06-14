248d5092d1690b953757465ce83757099bab57f7
Germán Lazcano <german.lazcano@alumnos.mdp.utn.edu.ar>
2022-06-13 22:09:47 -0300

    first commit

---
+++ src/test/java/com/utn/Deportes/DeportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.Deportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
